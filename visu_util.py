dcea1f42fc21296327f0f840c77991cc8e6559bc
James Braza <james.braza@synthego.com>
2021-11-10 17:10:00 -0800

    Documented visu_util.py

--- visu_util.py
+++ visu_util.py
@@ -1,5 +1,5 @@
-'''
-MIT License
+"""
+MIT License.
 
 Copyright (c) 2018 Wentao Yuan
 
@@ -20,16 +20,29 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+"""
 
+from typing import List, Optional, Sequence
+
+import numpy as np
 import open3d as o3d
-
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_pcd_three_views(filename, pcds, titles, suptitle='', sizes=None, cmap='Reds', zdir='y',
-                         xlim=(-0.3, 0.3), ylim=(-0.3, 0.3), zlim=(-0.3, 0.3)):
+def plot_pcd_three_views(
+    filename: str,
+    pcds: Sequence[np.ndarray],
+    titles: Sequence[str],
+    suptitle="",
+    sizes: Optional[List[float]] = None,
+    cmap="Reds",
+    zdir="y",
+    xlim=(-0.3, 0.3),
+    ylim=(-0.3, 0.3),
+    zlim=(-0.3, 0.3),
+):
+    """Save three views of the point clouds with the specified titles to the input filename."""
     if sizes is None:
         sizes = [0.5 for i in range(len(pcds))]
     fig = plt.figure(figsize=(len(pcds) * 3, 9))
@@ -38,21 +51,34 @@ def plot_pcd_three_views(filename, pcds, titles, suptitle='', sizes=None, cmap='
         azim = -45 + 90 * i
         for j, (pcd, size) in enumerate(zip(pcds, sizes)):
             color = pcd[:, 0]
-            ax = fig.add_subplot(3, len(pcds), i * len(pcds) + j + 1, projection='3d')
+            ax = fig.add_subplot(3, len(pcds), i * len(pcds) + j + 1, projection="3d")
             ax.view_init(elev, azim)
-            ax.scatter(pcd[:, 0], pcd[:, 1], pcd[:, 2], zdir=zdir, c=color, s=size, cmap=cmap, vmin=-1, vmax=0.5)
+            ax.scatter(
+                pcd[:, 0],
+                pcd[:, 1],
+                pcd[:, 2],
+                zdir=zdir,
+                c=color,
+                s=size,
+                cmap=cmap,
+                vmin=-1,
+                vmax=0.5,
+            )
             ax.set_title(titles[j])
             ax.set_axis_off()
             ax.set_xlim(xlim)
             ax.set_ylim(ylim)
             ax.set_zlim(zlim)
-    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.9, wspace=0.1, hspace=0.1)
+    plt.subplots_adjust(
+        left=0.05, right=0.95, bottom=0.05, top=0.9, wspace=0.1, hspace=0.1
+    )
     plt.suptitle(suptitle)
     fig.savefig(filename)
     plt.close(fig)
 
 
-def show_pcd(points):
+def show_pcd(points: np.ndarray) -> None:
+    """Visualize a point cloud in an interactive window."""
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
     o3d.visualization.draw_geometries([pcd])
