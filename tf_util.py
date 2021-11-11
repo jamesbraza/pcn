11b284a4a11ffdf96ac2b1e599d420438edce297
James Braza <james.braza@synthego.com>
2021-11-10 21:32:18 -0800

    Blackened, isorted, docformatted tf_util.py

--- tf_util.py
+++ tf_util.py
@@ -1,5 +1,5 @@
-'''
-MIT License
+"""
+MIT License.
 
 Copyright (c) 2018 Wentao Yuan
 
@@ -20,51 +20,62 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+"""
 
 import tensorflow as tf
-from pc_distance import tf_nndistance, tf_approxmatch
+
+from pc_distance import tf_approxmatch, tf_nndistance
 
 
 def mlp(features, layer_dims, bn=None, bn_params=None):
     for i, num_outputs in enumerate(layer_dims[:-1]):
         features = tf.contrib.layers.fully_connected(
-            features, num_outputs,
+            features,
+            num_outputs,
             normalizer_fn=bn,
             normalizer_params=bn_params,
-            scope='fc_%d' % i)
+            scope="fc_%d" % i,
+        )
     outputs = tf.contrib.layers.fully_connected(
-        features, layer_dims[-1],
+        features,
+        layer_dims[-1],
         activation_fn=None,
-        scope='fc_%d' % (len(layer_dims) - 1))
+        scope="fc_%d" % (len(layer_dims) - 1),
+    )
     return outputs
 
 
 def mlp_conv(inputs, layer_dims, bn=None, bn_params=None):
     for i, num_out_channel in enumerate(layer_dims[:-1]):
         inputs = tf.contrib.layers.conv1d(
-            inputs, num_out_channel,
+            inputs,
+            num_out_channel,
             kernel_size=1,
             normalizer_fn=bn,
             normalizer_params=bn_params,
-            scope='conv_%d' % i)
+            scope="conv_%d" % i,
+        )
     outputs = tf.contrib.layers.conv1d(
-        inputs, layer_dims[-1],
+        inputs,
+        layer_dims[-1],
         kernel_size=1,
         activation_fn=None,
-        scope='conv_%d' % (len(layer_dims) - 1))
+        scope="conv_%d" % (len(layer_dims) - 1),
+    )
     return outputs
 
 
 def point_maxpool(inputs, npts, keepdims=False):
-    outputs = [tf.reduce_max(f, axis=1, keepdims=keepdims)
-        for f in tf.split(inputs, npts, axis=1)]
+    outputs = [
+        tf.reduce_max(f, axis=1, keepdims=keepdims)
+        for f in tf.split(inputs, npts, axis=1)
+    ]
     return tf.concat(outputs, axis=0)
 
 
 def point_unpool(inputs, npts):
     inputs = tf.split(inputs, inputs.shape[0], axis=0)
-    outputs = [tf.tile(f, [1, npts[i], 1]) for i,f in enumerate(inputs)]
+    outputs = [tf.tile(f, [1, npts[i], 1]) for i, f in enumerate(inputs)]
     return tf.concat(outputs, axis=1)
 
 
@@ -84,10 +95,10 @@ def earth_mover(pcd1, pcd2):
 
 
 def add_train_summary(name, value):
-    tf.summary.scalar(name, value, collections=['train_summary'])
+    tf.summary.scalar(name, value, collections=["train_summary"])
 
 
 def add_valid_summary(name, value):
     avg, update = tf.metrics.mean(value)
-    tf.summary.scalar(name, avg, collections=['valid_summary'])
+    tf.summary.scalar(name, avg, collections=["valid_summary"])
     return update
