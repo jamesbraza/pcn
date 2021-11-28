dfc23aa6f670a264ce8b2e415412f7087877c161
James Braza <james.braza@synthego.com>
2021-11-27 18:07:48 -0800

    Type hinted return of lmdb_datflow

--- data_util.py
+++ data_util.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from typing import Tuple
 
 import numpy as np
 import tensorflow as tf
@@ -118,7 +119,7 @@ def lmdb_dataflow(
     output_size: int,
     is_training: bool,
     test_speed: bool = False,
-):
+) -> Tuple[DataFlow, int]:
     df: DataFlow = dataflow.LMDBSerializer.load(lmdb_path, shuffle=False)
     size = df.size()
     if is_training:
